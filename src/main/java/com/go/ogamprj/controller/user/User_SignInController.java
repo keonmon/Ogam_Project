2cd68449a51bf1358931fddaa6e097dafcc60854
YONGLEE\scolp <soillermoon@gmail.com>
2022-11-09 22:51:04 +0900

    회원가입페이지 수정, 컨트롤러 일부 추가

--- src/main/java/com/go/ogamprj/controller/user/User_SignInController.java
+++ src/main/java/com/go/ogamprj/controller/user/User_SignInController.java
@@ -1,7 +1,14 @@
 package com.go.ogamprj.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class User_SignInController {
+
+    @RequestMapping("/Signup")
+    public  String signup(){
+
+        return "user/loginPage/SignUp";
+    }
 }
