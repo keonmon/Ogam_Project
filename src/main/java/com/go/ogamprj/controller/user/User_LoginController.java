bf3eb27dbf919c58da8b7815bc51d52b7dad5d55
YONGLEE\scolp <soillermoon@gmail.com>
2022-11-09 21:32:35 +0900

    아이디 비밀번호 찾기 수정 / 회원가입 페이지

--- src/main/java/com/go/ogamprj/controller/user/User_LoginController.java
+++ src/main/java/com/go/ogamprj/controller/user/User_LoginController.java
@@ -1,7 +1,38 @@
 package com.go.ogamprj.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class User_LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+
+        return "/user/loginPage/login";
+    }
+
+    @RequestMapping("/Find")
+    public String Find(){
+
+        return "user/loginPage/Find";
+    }
+
+    @RequestMapping("/Find_id")
+    public String findid(){
+
+        return "user/loginPage/Find_id";
+    }
+
+    @RequestMapping("Find_pw")
+    public String findpw(){
+
+        return "user/loginPage/Find_pw";
+    }
+
+    @RequestMapping("/Signup")
+    public  String signup(){
+
+        return "user/loginPage/SignUp";
+    }
 }
