fc2d072cb266d402b3bc19b5de478d1081452eee
YONGLEE\scolp <soillermoon@gmail.com>
2022-11-11 20:03:02 +0900

    로그인 기능 세션 제외 완성

--- src/main/java/com/go/ogamprj/controller/user/User_LoginController.java
+++ src/main/java/com/go/ogamprj/controller/user/User_LoginController.java
@@ -1,27 +1,48 @@
 package com.go.ogamprj.controller.user;
 
+import com.go.ogamprj.sevice.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class User_LoginController {
 
+    @Autowired
+    LoginService loginService;
 
     @RequestMapping("/loginPage")
-    public String loginPage(){
-
+    public String loginPage(@RequestParam(required = false, defaultValue = "") String member_email, Model model){
+        model.addAttribute("member_email", member_email);
         return "user/loginPage/loginPage";
     }
 
     @RequestMapping("/login")
-    public String login(HttpServletRequest request){
+    public String login(HttpServletRequest request, @RequestParam String member_email, @RequestParam String member_pw){
 
-        return "redirect:/loginPage";
+       boolean loginvalid = loginService.loginvalid(member_email, member_pw);
 
+
+      if(loginvalid == true) {
+
+         return "user/userDashboard/userDashboard";
+      } else{
+
+       return "redirect:/loginPage";
+        }
     }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().removeAttribute("member_email");
+        return "redirect:/loginPage";
+    }
+
 
     @RequestMapping("/Find")
     public String Find(){
