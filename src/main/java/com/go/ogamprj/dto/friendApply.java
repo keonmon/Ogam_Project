bcd534a6d281b07294f5e02d92b450ce19f02fe0
Leehojun12 <dlghwns82@naver.com>
2022-11-12 20:41:07 +0900

    commit - 모두의 일기 진행중

--- src/main/java/com/go/ogamprj/dto/friendApply.java
+++ src/main/java/com/go/ogamprj/dto/friendApply.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
