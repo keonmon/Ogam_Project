c270193ad9457287f52dace77888222b7a6e63d0
Leehojun12 <dlghwns82@naver.com>
2022-11-13 18:41:21 +0900

    commit

--- src/main/java/com/go/ogamprj/controller/user/User_FrndDiaryController.java
+++ src/main/java/com/go/ogamprj/controller/user/User_FrndDiaryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
