5829dd28d8b5a2e88a8ed29e9ab89d4c7a71e7cb
Keonmon <dlzjsgml12@gmail.com>
2022-11-14 15:20:38 +0900

    아이디/ 비밀번호 찾기 기능 구현 완료

--- src/main/java/com/go/ogamprj/mapper/LoginMapper.java
+++ src/main/java/com/go/ogamprj/mapper/LoginMapper.java
@@ -2,18 +2,21 @@ package com.go.ogamprj.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.Map;
 
 @Mapper
 public interface LoginMapper {
 
-   @Select("SELECT MEMBER_EMAIL, MEMBER_PW FROM MEMBER WHERE MEMBER_EMAIL = #{member_email}")
+   @Select("SELECT * FROM MEMBER WHERE MEMBER_EMAIL = #{member_email}")
    Map<String,Object> memberSelectOne(String member_email);
 
    @Select("SELECT member_email FROM MEMBER WHERE MEMBER_BIRTH = #{member_birth} AND MEMBER_PHONE = #{member_phone}")
    String idFindSelectOne(String member_birth, String member_phone);
 
-   @Select("SELECT COUNT(*) FROM MEMBER WHERE MEMBER_EMAIL = #{member_email} AND MEMBER_PHONE = #{member_phone}")
-    int findpw(String member_email, String member_phone);
+
+
+   @Update("update member set MEMBER_PW = #{uuid} where MEMBER_EMAIL = #{member_email}")
+   void updateUserPassword(String uuid, String member_email);
 }
